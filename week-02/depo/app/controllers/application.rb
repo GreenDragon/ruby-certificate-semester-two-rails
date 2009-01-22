40b7762164880ebf1d674be9859acea2cf091c57
GreenDragon <elf-github@arashi.com>
2009-01-21 23:57:50 -0800

    I say we take off and nuke the entire site from orbit. It's the only way to be sure.

---
+++ week-02/depo/app/controllers/application.rb
@@ -0,0 +1,15 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '1f3bc524d7649f26e22d4b628f526f4a'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+end
