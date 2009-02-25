d632c12bffeb26576265ecc076ab13ab6190ad90
GreenDragon <elf-github@arashi.com>
2009-02-25 01:11:42 -0800

    Initial Error Driven Development passes

--- rubyholic/app/models/group.rb
+++ rubyholic/app/models/group.rb
@@ -1,2 +1,21 @@
 class Group < ActiveRecord::Base
+  has_many    :locations
+
+  def self.sort_by_group_name
+    Group.find(:all, :order => "name")
+  end
+
+  def self.sort_by_location_name
+    Group.find(:all, :include => :locations, :order => "locations.name")
+  end
+
+  #def self.sort_by_location_name
+  #  Group.find_by_sql "
+  #    SELECT groups.name AS group_name, 
+  #      locations.name AS location_name,
+  #      latitude, longitude
+  #    FROM locations 
+  #    INNER JOIN groups ON locations.group_id = groups.id 
+  #    ORDER BY locations.name"
+  #end
 end
--- rubyholic/db/migrate/20090225082154_create_locations.rb
+++ rubyholic/db/migrate/20090225082154_create_locations.rb
@@ -5,6 +5,7 @@ class CreateLocations < ActiveRecord::Migration
       t.string :address
       t.float :latitude
       t.float :longitude
+
       t.integer :group_id
 
       t.timestamps
